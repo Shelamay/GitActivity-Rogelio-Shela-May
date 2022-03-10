3c5b9d029a173fa1bf7fe88e6df9f661655c4679
RogelioShelaMay <shelamayrogelio@gmail.com>
2022-03-10 19:46:17 +0700

    My Educational Background added

--- git-python-rogelio.py
+++ git-python-rogelio.py
@@ -1,3 +1,7 @@
 print("Shela May Revilla Rogelio")
 print("Hupi, Santa Cruz, Marinduque")
 print("October 4, 2001")
+print("Marinduque State College")
+print("Bachelor of Science Information Technology")
+print("2nd Year College")
+print("Section 2-A")
